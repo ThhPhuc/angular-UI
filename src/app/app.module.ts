7503cb8f9b8a653a85a236339f8485b9ca5e353b
Nguyen Huy <huy.nguyen@netpower.no>
2022-09-27 17:12:32 +0700

    structure folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './components/input/input.component';
+import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    ButtonComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
